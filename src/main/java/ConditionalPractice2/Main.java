8e2c530e09eb1baff50b376c14b12490478c1bdf
modengann <99417989+modengann@users.noreply.github.com>
2022-09-09 09:49:45 -0400

    init

---
+++ src/main/java/ConditionalPractice2/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Overall task: Take three numbers from the user and print the greatest number.
+ * 
+ * Include:
+ * Messages to the user, asking them to enter each number.
+ * Some output to show the result. For example: "The greatest number was ...", where
+ * the ... are replaced by the result
+
+ */
+
+package ConditionalPractice2;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+
+    }
+    
+}
